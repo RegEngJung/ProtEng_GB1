4e1086769ff007165a4ec567481daa64cc3bee82
LaneYahtzee <92941983+LaneYahtzee@users.noreply.github.com>
2024-02-19 11:43:09 -0600

    Delete 3. Scripts and Dependencies for Sequence Input Feature Generation directory
    Revision. Replaced depreciated files with functional variants.

--- 3. Scripts and Dependencies for Sequence Input Feature Generation/Wittmann_Data_Pipeline/individual scripts/save_numpy_to_text.py
+++
@@ -1,8 +0,0 @@
-"""Script converts numpy file to txt file"""
-import numpy as np
-INPUT = None #msa_transformer.npy"
-OUTPUT = None #msa_transformer.csv
-
-three_dim = np.load(INPUT)
-output_numpy = np.ndarray.flatten(three_dim)
-np.savetxt(OUTPUT, output_numpy, fmt='%f', delimiter=',')
